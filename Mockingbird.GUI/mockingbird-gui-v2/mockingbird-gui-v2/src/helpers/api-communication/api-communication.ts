8076afac65a07960c33cc740a495e0818f6593ee
krzysztofmalcher <krzysztof.malcher@auctane.com>
2023-11-30 07:40:58 +0100

    first attempt to comunicate with api

---
+++ Mockingbird.GUI/mockingbird-gui-v2/mockingbird-gui-v2/src/helpers/api-communication/api-communication.ts
@@ -0,0 +1,18 @@
+import type { AxiosRequestConfig, AxiosResponse } from 'axios';
+import { Axios } from 'axios';
+
+export const GetApiResponse = async (requestConfig: AxiosRequestConfig): Promise<AxiosResponse> => {
+
+
+    const axios = new Axios(requestConfig);
+    let response: AxiosResponse;
+
+    try {
+        response = await axios.request(requestConfig);
+    }
+    catch(e) {
+        throw new Error(`an unexpected error occurred during api communication with message`);
+    }
+
+    return response;
+};
