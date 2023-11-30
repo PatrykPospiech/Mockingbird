4d1ec08dcc41cea6d2a1d3bf6fc836ee95743ddd
mmilowska <magdalena.milowska@auctane.com>
2023-11-30 14:07:52 +0100

    Changing base url

--- Mockingbird.GUI/mockingbird-gui/src/helpers/api-communication/config.ts
+++ Mockingbird.GUI/mockingbird-gui/src/helpers/api-communication/config.ts
@@ -1,2 +1,2 @@
-export const BACKEND_URL : string = 'http://localhost:4044';
-export const CARRIER_URL: string = `${BACKEND_URL}/carrier`;
+export const BACKEND_URL : string = 'http://localhost:8080';
+export const CARRIER_URL: string = `${BACKEND_URL}/mockingbird/carrier`;
