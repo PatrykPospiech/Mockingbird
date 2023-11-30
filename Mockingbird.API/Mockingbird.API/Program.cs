365cdf1799daa32c26b269b601e9b8c6286ef7c7
Patryk Pospiech <patryk.pospiech@auctane.com>
2023-11-30 11:25:06 +0100

    some fixes to dockerfile and API configuration

--- Mockingbird.API/Mockingbird.API/Program.cs
+++ Mockingbird.API/Mockingbird.API/Program.cs
@@ -1,3 +1,4 @@
+using System.Net;
 using Mockingbird.API;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -5,7 +6,37 @@ var app = builder.Build();
 
 #region MockEndpoints
 
-app.MapPost("{endpoint}", (string endpoint) => { return DataSource.GetResponse(endpoint).Value; });
+app.MapPost("{endpoint}", (string endpoint, HttpContext context) =>
+{
+    Console.WriteLine("Post mock endpoint executing....");
+    var url = "https://dpdservicesdemo.dpd.com.pl/DPDPackageObjServicesService/DPDPackageObjServices";
+
+    HttpRequest original = context.Request;
+    HttpWebRequest newRequest = (HttpWebRequest)WebRequest.Create(url);
+
+    newRequest.ContentType = original.ContentType;
+    newRequest.Method = original.Method;
+
+    //var originalStream = ReadFully(original.Body);
+    Console.WriteLine("data updating....");
+
+    // using (BinaryReader br = new BinaryReader(original.Body))
+    // {
+    //     originalStream = br.ReadBytes((int)original.Body.Length);
+    // }
+
+
+    //
+    // Stream reqStream = newRequest.GetRequestStream();
+    // reqStream.Write(originalStream, 0, originalStream.Length);
+    // reqStream.Close();
+    //
+    //
+    // Console.WriteLine("executing request....");
+    // var response = newRequest.GetResponse();
+    // Console.WriteLine("Response" + response);
+    return DataSource.GetResponse(endpoint).Value;
+});
 app.MapGet("{endpoint}", (string endpoint) => { return DataSource.GetResponse(endpoint).Value; });
 app.MapPut("{endpoint}", (string endpoint) => { return DataSource.GetResponse(endpoint).Value; });
 app.MapDelete("{endpoint}", (string endpoint) => { return DataSource.GetResponse(endpoint).Value; });
@@ -14,3 +45,14 @@ app.MapDelete("{endpoint}", (string endpoint) => { return DataSource.GetResponse
 
 
 app.Run();
+
+
+
+// static byte[] ReadFully(Stream input)
+// {
+//     using (MemoryStream ms = new MemoryStream())
+//     {
+//         input.CopyTo(ms);
+//         return ms.ToArray();
+//     }
+// }
